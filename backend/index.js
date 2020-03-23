cb39732c7e42dea8dd73cb984298770828de5842
GabrieleSuzart <gabrielesuzart@gmail.com>
2020-03-23 20:53:48 -0300

    Reponse in json

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,19 @@ const express = require('express')
 
 const app = express()
 
+const user = {
+  name: 'Marília Suzart',
+  job: 'Fullstack developer',
+  github: 'https://github.com/GabrieleSuzart',
+  skills: ['JavaScript, ES6, React, Angular, CSS3, HTML5'],
+  address: {
+    city: 'São Paulo',
+    state: 'SP'
+  }
+}
+
 app.get('/', (request, response) => {
-  return response.send('Hello World')
+  return response.json(user)
 })
 
 app.listen(3333)
